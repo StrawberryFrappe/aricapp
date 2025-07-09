bf9e4047ea24771e533c4384b9727733ffaee039
Strawberry Frappé <j.saez18@ufromail.cl>
2025-07-09 02:29:56 -0400

    fix: reset Metro config to default
    - Remove custom resolver configurations that were causing bundling issues
    - Simplify to standard Expo Metro config
    - Fix Android bundling failures

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Exclude problematic directories that cause VS Code issues
-config.resolver.blacklistRE = /node_modules\/.*\/android\/.*|node_modules\/.*\/ios\/.*/;
-
-// Exclude specific paths that cause file reading errors
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
 module.exports = config;
